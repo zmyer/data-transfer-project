930329a2b453ef9474e385f1e2d9d02f76014f26
Anna Olson <olsona@google.com>
2018-03-23 10:10:00 -0500

    Starting instagram work

---
+++ extensions/data-transfer/portability-data-transfer-instagram/src/main/java/org/dataportabilityproject/datatransfer/instagram/photos/InstagramPhotosExporter.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2018 The Data Transfer Project Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.dataportabilityproject.datatransfer.instagram.photos;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.api.client.http.HttpTransport;
+import com.google.common.annotations.VisibleForTesting;
+import org.dataportabilityproject.spi.transfer.provider.ExportResult;
+import org.dataportabilityproject.spi.transfer.provider.Exporter;
+import org.dataportabilityproject.spi.transfer.types.ExportInformation;
+import org.dataportabilityproject.types.transfer.auth.AuthData;
+import org.dataportabilityproject.types.transfer.models.photos.PhotosContainerResource;
+
+public class InstagramPhotosExporter implements Exporter<AuthData, PhotosContainerResource> {
+
+  // TODO(olsona): is this the mapper we want to use?
+  private static final ObjectMapper MAPPER =
+      new ObjectMapper().configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+
+  private final HttpTransport httpTransport;
+
+  @VisibleForTesting
+  InstagramPhotosExporter(HttpTransport httpTransport) {
+    this.httpTransport = httpTransport;
+  }
+
+  @Override
+  public ExportResult<PhotosContainerResource> export(AuthData authData) {
+    return null;
+  }
+
+  @Override
+  public ExportResult<PhotosContainerResource> export(AuthData authData,
+      ExportInformation exportInformation) {
+    return null;
+  }
+}
